2b6715c64451980b12a4c0e515ebb025d9c62c11
Animesh <6973916+bansriyar@users.noreply.github.com>
2018-01-18 14:18:15 +0530

    Fixed enum issue in the Swagger spec

--- rsearch_client/models/document_type_fields.py
+++ rsearch_client/models/document_type_fields.py
@@ -96,12 +96,6 @@ class DocumentTypeFields(object):
         :param type: The type of this DocumentTypeFields.  # noqa: E501
         :type: str
         """
-        allowed_values = ["string", "text", "float", "integer", "date", "location", "fixed"]  # noqa: E501
-        if type not in allowed_values:
-            raise ValueError(
-                "Invalid value for `type` ({0}), must be one of {1}"  # noqa: E501
-                .format(type, allowed_values)
-            )
 
         self._type = type
 
